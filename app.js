040b59979b020405e68dbf410250f5338bf0782a
pratikpakhale <pratikpakhale20@gmail.com>
2023-06-23 22:15:45 +0530

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const HTMLtoJSX = require('htmltojsx')
+
+const app = express()
+
+app.use(express.text())
+
+app.post('/', (req, res) => {
+  const html = req.body.replace(/class="([^"]*)"/g, (match, classValue) => {
+    const modifiedClassValue = classValue
+      .split(' ')
+      .filter(word => !word.includes('dark'))
+      .join(' ')
+    return `class="${modifiedClassValue}"`
+  })
+
+  const converter = new HTMLtoJSX({
+    createClass: false,
+  })
+  const jsx = converter.convert(html)
+  res.send(jsx)
+})
+
+app.listen(8080, () => console.log('Listening on port 8080!'))
